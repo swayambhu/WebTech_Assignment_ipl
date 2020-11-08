35ceb720fa6f5ca34b75bb3869d4f5ec47f5e13c
swayambhu <swayambhu43@gmail.com>
2020-11-08 14:17:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+
+//var num1,num2,num3;
+//
+//num1 = window.prompt('Please enter number 1');
+//
+//num2 = window.prompt('Please enter number 2');
+//
+//num3 = window.prompt('Please enter number 3');
+//var sum = parseInt(num1)+ parseInt(num2) + parseInt(num3);
+//console.log(sum);
